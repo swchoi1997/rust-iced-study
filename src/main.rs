218b4ed7a4894202e1c48e12b88c10c1c94487cc
SangWon <swchoi1997@naver.com>
2024-09-21 19:34:11 +0900

    feat: inputtext example2

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use iced::{
     },
     Font, Sandbox, Settings
 };
+use base64::{prelude::BASE64_STANDARD, Engine};
 
 // 메인 함수: 프로그램의 진입점
 // iced::Result를 반환하여 애플리케이션 실행 중 발생할 수 있는 오류를 처리합니다.
@@ -21,27 +22,17 @@ fn main() -> iced::Result {
 
 #[derive(Debug, Clone)]
 enum MyAppMessage{
-    Update3(String),
-    Update4(String),
-    Update5(String),
-    Paste5(String),
-    Update6(String),
-    Submit6,
-    Update7(String),
-    Update11(String),
+    UpdateAppStr(String),
+    SubmitButton
+
 }
 
 // MyApp 구조체 정의: 애플리케이션의 상태를 나타냅니다.
 #[derive(Default)]
 struct MyApp{
-    text3: String,
-    text4: String,
-    text5: String,
-    info5: String,
-    text6: String,
-    info6: String,
-    text7: String,
-    text11: String,
+    app_str: String,
+    submit_text: String,
+
 }
 
 // MyApp에 대한 Sandbox 트레이트 구현
@@ -64,23 +55,14 @@ impl Sandbox for MyApp {
     // 현재는 아무 동작도 하지 않습니다.
     fn update(&mut self, _message: Self::Message) {
         match _message {
-            MyAppMessage::Update3(s) => self.text3 = s,
-            MyAppMessage::Update4(s) => self.text4 = s,
-            MyAppMessage::Update5(s) => {
-                self.text5 = s;
-                self.info5 = "".into();
+            MyAppMessage::UpdateAppStr(app_str) => {
+                self.app_str = app_str;
             },
-            MyAppMessage::Paste5(s) => {
-                self.text5 = s;
-                self.info5 = "Pasted".into();
-            },
-            MyAppMessage::Update6(s) => {
-                self.text6 = s;
-                self.info6 = "".into();
-            },
-            MyAppMessage::Submit6 => self.info6 = "Submitted".into(),
-            MyAppMessage::Update7(s) => self.text7 = s,
-            MyAppMessage::Update11(s) => self.text11 = s,
+            MyAppMessage::SubmitButton => {
+                self.submit_text = BASE64_STANDARD.encode(self.app_str.clone());
+                self.app_str = String::from("");
+                
+            }
         }
     }
 
@@ -88,38 +70,12 @@ impl Sandbox for MyApp {
     // 현재는 단순히 "Hello World" 텍스트를 표시합니다.
     fn view(&self) -> iced::Element<Self::Message> {
         column![
-            text_input("Construct from function", ""),
-            TextInput::new("Construct from struct", ""),
-            text_input("Emable text input", self.text3.as_str())
-                .on_input(|s| MyAppMessage::Update3(s)),
-            text_input("Shorter on input", self.text4.as_str())
-                .on_input(MyAppMessage::Update4),
-            text_input("Press Ctrl/Cmd + v", self.text5.as_str())
-                .on_input(MyAppMessage::Update5)
-                .on_paste(MyAppMessage::Paste5),
-            text(self.info5.as_str()),
-            text_input("Press enter", self.text6.as_str())
-                .on_input(MyAppMessage::Update6)
-                .on_submit(MyAppMessage::Submit6),
-            text(self.info6.as_str()),
-            text_input("Password", self.text7.as_str())
-                .secure(true)
-                .on_input(MyAppMessage::Update7),
-            text_input("Different font", "")
-                .font(Font {
-                    family: Family::Fantasy,
-                    ..Font::DEFAULT
-                }),
-            text_input("Larger text", "").size(24),
-            text_input("With padding", "").padding(20),
-            text_input("Icon", self.text11.as_str())
-                .icon(Icon{
-                    font: Font::DEFAULT,
-                    code_point: '\u{2705}',
-                    size: None,
-                    spacing: 10 as f32,
-                    side: Side::Left,
-                }).on_input(MyAppMessage::Update11),
+            text("Input Text"),
+            text_input("Any", self.app_str.as_str())
+                .on_input(MyAppMessage::UpdateAppStr)
+                .on_paste(MyAppMessage::UpdateAppStr),
+            button("Encode").on_press(MyAppMessage::SubmitButton).padding(5),
+            text(self.submit_text.as_str()),
         ]
         .into()
     }
